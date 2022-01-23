ca0c9e45b342a333be57bff7c7cdb333a838c6ef
FOREST-QIN <867244098@qq.com>
2022-01-22 19:03:08 -0500

    Initial commit

---
+++ src/main/java/com/example/twicthrecommandation/TwicthRecommandationApplication.java
@@ -0,0 +1,13 @@
+package com.example.twicthrecommandation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwicthRecommandationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TwicthRecommandationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/twicthrecommandation/TwicthRecommandationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twicthrecommandation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwicthRecommandationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
